bec39ace7bf50b504efead169b1cffe6d94d32cb
cruzayye <johnnyc010392@gmail.com>
2019-02-19 17:07:20 -0800

    footer takes input

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,30 @@ import React, { PureComponent } from 'react';
 export default class Demo extends PureComponent {
 
   state = {
-    text: ''
+    header: '',
+    footer: ''
   };
 
  handleChange = ({ target }) => {
-   this.setState({ [target.name]: target.value }, () => {
-     console.log(target.value);
-   });
+   this.setState({ [target.name]: target.value });
  };
 
 
  
 
+
+
+
+ 
+
  render() {
-   const { text } = this.state;
+   const { header, footer } = this.state;
    return (
       <>
-        <input type="text" name="text" value={text} onChange={this.handleChange}></input>
-        <header>{text}</header>
-        <footer>footer</footer>
+        <input type="text" name="header" value={header} onChange={this.handleChange}></input>
+        <header>{header}</header>
+        <input type="text" name="footer" value={footer} onChange={this.handleChange} ></input>
+        <footer>{footer}</footer>
       </>
    );
  }
