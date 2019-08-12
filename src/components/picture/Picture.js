b1b8c3e9ba63cc98634d47a4277221a1991a43cb
cruzayye <johnnyc010392@gmail.com>
2019-08-12 13:04:53 -0700

    image layout laid out

--- src/components/picture/Picture.js
+++ src/components/picture/Picture.js
@@ -5,11 +5,11 @@ import styles from './Picture.css';
 const Picture = ({ img, onChange }) => {
   return (
     <article className={styles.Picture}>
-      <section>
+      <section className={styles.first}>
         <h1>Copy image url or upload your own image</h1>
         <input type="img" name="img" value={img} onChange={onChange} placeholder="image goes here" />
       </section>
-      <section className={styles.middle}>
+      <section className={styles.second}>
         <img src={img}/>
       </section>
     </article>
